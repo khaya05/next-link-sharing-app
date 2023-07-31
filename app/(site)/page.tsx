ae06b545cd7e4d72da856c89d6b5d4bb03c03b58
khaya05 <khayamnyandu5@gmail.com>
2023-07-31 06:30:49 +0200

    initial commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className="h-full grid place items center">
+      <Link href="/login">Login</Link>
+    </main>
+  );
+}
